ae8a2fe3305c1ce949e4bfad4faf26c7cb6e6ee6
Sijia Ai <aixxx033@umn.edu>
2015-03-12 16:15:28 -0500

    partial edit added

--- contact-viewer.js
+++ contact-viewer.js
@@ -1,4 +1,4 @@
-var _apiKey='admin';
+var _apiKey='grumpy';
 var _contacts = {};
 var _contactId = null;
 
@@ -45,21 +45,61 @@ $(document).on('pagebeforeshow','#details-page',function() {
   $('.contact-twitterid').text(contact.twitterId)
 })
 
-$(document).on('pagebeforeshow','#new-page',function() {
-	$.get(
-    'http://contacts.tinyapollo.com/contacts?key='+_apiKey,
+$(document).on('pagebeforeshow','#edit-page',function() {
+  var contact = _contacts[_contactId]
+  $('.contact-name').val(contact.name)
+  $('.contact-title').val(contact.title)
+  $('.contact-email').val(contact.email)
+  $('.contact-phone').val(contact.phone)
+  $('.contact-twitterid').val(contact.twitterId)
+})
+
+$(document).on('click','#edit-button',function() {
+	var name=$('.contact-name').val()
+	var title=$('.contact-title').val()
+	var email=$('.contact-email').val()
+	var phone=$('.contact-phone').val()
+	var twitterId=$('.contact-twitterid').val()
+
+	$.put(
+     'http://contacts.tinyapollo.com/contacts/'+ +'?key='+_apiKey+
+		'&name='+name+
+		'&title='+title+
+		'&email='+email+
+		'&phone='+phone+
+		'&twitterId'+twitterId,
+
       function(result) {
-        _contacts = {};
-        //console.log("there are "+ result.contacts.length + " contacts");
-          for (i in result.contacts) {
-              var contact = result.contacts[i];
-              _contacts[contact._id] = contact;
-              contactList.append('<li><a href="#details-page" ' +
-                'data-contact-id="' +
-                contact._id + '">' +
-                contact.name + '</a></li>')
-          }
-          contactList.listview('refresh');
+		  if(result.status=="error"){
+			alert("message: "+result.message+" contact: "+result.contact)	 
+		  }else{
+			alert("edit success!")
+		  }
+      }
+  )
+})
+
+$(document).on('click','#new-button',function() {
+	var name=$('#new-name').val()
+	var title=$('#new-title').val()
+	var email=$('#new-email').val()
+	var phone=$('#new-phone').val()
+	var twitterId=$('#new-twitterId').val()
+
+	$.post(
+     'http://contacts.tinyapollo.com/contacts?key='+_apiKey+
+		'&name='+name+
+		'&title='+title+
+		'&email='+email+
+		'&phone='+phone+
+		'&twitterId'+twitterId,
+
+      function(result) {
+		  if(result.status=="error"){
+			alert("message: "+result.message+" contact: "+result.contact)	 
+		  }else{
+			alert("insert success!")
+		  }
       }
   )
 })
